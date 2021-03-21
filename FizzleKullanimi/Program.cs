bffa0f5496e53d1f53557378a3d974c233745b29
Pairs34 <34889012+Pairs34@users.noreply.github.com>
2021-03-21 15:00:26 +0300

    First Init

---
+++ FizzleKullanimi/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Fizzler.Systems.HtmlAgilityPack;
+using HtmlAgilityPack;
+
+namespace FizzleKullanimi
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var web = new HtmlWeb();
+
+            var document =  web.Load("https://www.teknosa.com/iphone-12-c-100001001013");
+
+            var productItems = document.DocumentNode.QuerySelectorAll("div.product-item");
+
+            foreach (var productItem in productItems)
+            {
+                
+                Console.WriteLine(productItem.Attributes["data-product-url"].Value);
+            }
+        }
+    }
+}
